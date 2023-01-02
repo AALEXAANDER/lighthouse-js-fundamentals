55c16a25ba086c63f7ba6b2d76370deb653dc296
Adam Marx <adamalexmarx@gmail.com>
2023-01-01 23:26:05 -0800

    Arrays and objects

---
+++ judge-vegetable.js
@@ -0,0 +1,31 @@
+const judgeVegetable = function (vegetables, metric) {
+    let bestVegetable = vegetables[0];
+    for (let i = 0; i < vegetables.length; i++) {
+      if (vegetables[i][metric] > bestVegetable[metric]){
+        bestVegetable = vegetables[i]
+      }
+    }
+    return bestVegetable.submitter;
+  }
+  
+  const vegetables = [
+    {
+      submitter: 'Old Man Franklin',
+      redness: 10,
+      plumpness: 5
+    },
+    {
+      submitter: 'Sally Tomato-Grower',
+      redness: 2,
+      plumpness: 8
+    },
+    {
+      submitter: 'Hamid Hamidson',
+      redness: 4,
+      plumpness: 3
+    }
+  ]
+  
+  const metric = 'redness'
+  
+  judgeVegetable(vegetables, metric)
